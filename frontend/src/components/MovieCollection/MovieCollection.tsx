3a182ef6a4142306aa1a2e7f4fc5c494bd29be55
adam3norton <adam3norton@gmail.com>
2023-04-07 23:57:41 -0600

    UseEffect, fetch, useState, etc

--- frontend/src/components/MovieCollection/MovieCollection.tsx
+++ frontend/src/components/MovieCollection/MovieCollection.tsx
@@ -1,21 +1,35 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import movieData from '../../assets/MovieData.json';
 import './MovieCollection.css';
+import { Movie } from '../../types/movie';
 
 const MovieCollection: React.FC = () => {
+
+  const [movieData, setMovieData] = useState<Movie[]>([]);
+
+  const fetchMovies = async () => {
+    const res = await fetch('https://localhost:4000/movies')
+    const temp = await res.json();
+    setMovieData(temp);
+  }
+
+  useEffect(() => {
+    fetchMovies();
+  }, []);
+
   return (
     <section style={{ paddingTop: '40px' }}>
       <h1>Movie Collection</h1>
       <section className="movieGrid">
-          {movieData.MovieData.map((movie, index) => (
-            <div className="movieCard" key={index}>
-              <h2>{movie.Title}</h2>
-              <p>Category: {movie.Category}</p>
-              <p>Director: {movie.Director}</p>
-              <p>Rating: {movie.Rating}</p>
-              <p>Edited: {movie.Edited}</p>
-            </div>
-          ))}
+        {movieData.map((movie, index) => (
+          <div className="movieCard" key={index}>
+            <h2>{movie.Title}</h2>
+            <p>Category: {movie.Category}</p>
+            <p>Director: {movie.Director}</p>
+            <p>Rating: {movie.Rating}</p>
+            <p>Edited: {movie.Edited}</p>
+          </div>
+        ))}
       </section>
     </section>
   );
---
+++ frontend/src/types/movie.ts
@@ -0,0 +1,8 @@
+export type Movie = {
+    movieId: number;
+    Title: string;
+    Director: string;
+    Category: string;
+    Rating: string;
+    Edited: boolean;
+}
